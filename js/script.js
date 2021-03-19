33cdde2e1619de274f887d113a5c1168db11f079
Mauro <maurogheco@gmail.com>
2021-03-19 12:55:11 -0300

    js script for social icons mobile version added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
-let socialDiv = document.querySelector('.social');
-let shareMobileIcon = document.querySelector('.share-mobile');
+const socialDiv = document.querySelector('.social');
+const shareMobileIcon = document.querySelector('.share-mobile');
 
-socialDiv.setAttribute(
-  'style',
-  'background-color: var(--very-dark-grayish-blue)'
-);
+let hidden = true;
 
-socialDiv.firstChild.nextSibling.classList.replace('user-info', 'social-menu');
+const socialMenu = () => {
+  if (hidden) {
+    socialDiv.classList.add('turn-dark');
 
-socialDiv.firstChild.nextSibling.innerHTML =
-  '<span class="share-title">share</span><div class="social-icons"><img id="fb-icon" src="images/icon-facebook.svg" alt="facebook icon"><img id="tw-icon" src="images/icon-twitter.svg" alt="twitter icon"><img id="pint-icon" src="images/icon-pinterest.svg" alt="pinterest icon"></div>';
+    socialDiv.firstChild.nextSibling.classList.replace(
+      'user-info',
+      'social-menu'
+    );
 
-shareMobileIcon.classList.add('active');
+    socialDiv.firstChild.nextSibling.innerHTML =
+      '<span class="share-title">share</span><div class="social-icons"><img id="fb-icon" src="images/icon-facebook.svg" alt="facebook icon"><img id="tw-icon" src="images/icon-twitter.svg" alt="twitter icon"><img id="pint-icon" src="images/icon-pinterest.svg" alt="pinterest icon"></div>';
+
+    shareMobileIcon.classList.add('active-button');
+
+    hidden = !hidden;
+  } else {
+    socialDiv.classList.remove('turn-dark');
+
+    socialDiv.firstChild.nextSibling.classList.replace(
+      'social-menu',
+      'user-info'
+    );
+
+    socialDiv.firstChild.nextSibling.innerHTML = `<div class="user-info">
+          <img
+            class="avatar"
+            src="images/avatar-michelle.jpg"
+            alt="avatar person"
+          />
+          <div class="name-date">
+            <h3>Michelle Appleton</h3>
+            <span>28 Jun 2020</span>
+          </div>
+        </div>`;
+
+    shareMobileIcon.classList.remove('active-button');
+
+    hidden = !hidden;
+  }
+};
+
+shareMobileIcon.addEventListener('click', socialMenu);
